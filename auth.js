d63a1a8c85993e3fac573b1d2fb25de24d258156
Bruno Krebs <bruno.krebs@auth0.com>
2018-07-24 09:25:34 -0300

    adding trailing semicolons; fixing style;

--- auth.js
+++ auth.js
@@ -1,11 +1,11 @@
 const passport = require('passport');
 const Auth0Strategy = require('passport-auth0');
 
-passport.use( new Auth0Strategy({
+passport.use(new Auth0Strategy({
     domain: 'AUTHO DOMAIN NAME',
     clientID: 'AUTHO CLIENT ID',
     clientSecret: 'AUTHO CLIENT SECRET',
     callbackURL: 'http://localhost:3000/callback'
   }, (accessToken, refreshToken, extraParams, profile, done) => {
     return done(null, profile);
-}))
+}));
