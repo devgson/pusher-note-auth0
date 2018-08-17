b80688d4d5bdeb232f35b558a27ed1beac660b17
Godson <godsonobielum@yahoo.com>
2018-08-17 12:45:29 +0100

    Added local Authorization

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,29 +1,32 @@
 const textSyncInstance = new TextSync({
-  instanceLocator: 'v1:us1:27293868-2246-4359-baa9-0ff2a8fe02ce',
+  instanceLocator: "v1:us1:27293868-2246-4359-baa9-0ff2a8fe02ce"
 });
 
-const docID = document.URL.slice(document.URL.lastIndexOf('/') + 1);
+const docID = document.URL.slice(document.URL.lastIndexOf("/") + 1);
 
 const editor = textSyncInstance.createEditor({
   docId: docID,
-  element: '#text_editor',
+  element: "#text_editor",
   cursorLabelsAlwaysOn: true,
-  authEndpoint: 'https://pusher-authenticate.herokuapp.com/textsync/tokens',
+  authEndpoint: "http://localhost:3000/textsync/tokens",
   userName: user,
 
-  onCollaboratorsJoined: (users) => {
-    const activeUsers = document.querySelector('.active_users ul');
-    users.forEach((value) => {
-      activeUsers.insertAdjacentHTML('beforeend', `<li id='${value.siteId}'>${value.name}</li>`);
-    })
+  onCollaboratorsJoined: users => {
+    const activeUsers = document.querySelector(".active_users ul");
+    users.forEach(value => {
+      activeUsers.insertAdjacentHTML(
+        "beforeend",
+        `<li id='${value.siteId}'>${value.name}</li>`
+      );
+    });
   },
 
-  onCollaboratorsLeft: (users) => {
-    const activeUsers = document.querySelectorAll('.active_users ul li');
-    users.forEach((value) => {
-      activeUsers.forEach((element) => {
+  onCollaboratorsLeft: users => {
+    const activeUsers = document.querySelectorAll(".active_users ul li");
+    users.forEach(value => {
+      activeUsers.forEach(element => {
         if (element.id === value.siteId) element.remove();
-      })
-    })
+      });
+    });
   }
 });
